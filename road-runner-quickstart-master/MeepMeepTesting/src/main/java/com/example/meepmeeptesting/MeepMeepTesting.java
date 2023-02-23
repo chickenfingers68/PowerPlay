3ad8d2126ab0a301394aeee5efcc91ce09ddafc9
chickenfingers68 <jackienguyen796@gmail.com>
2023-02-22 22:10:12 -0800

    Removed test comments in MeepMeepTesting

--- road-runner-quickstart-master/MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ road-runner-quickstart-master/MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -54,7 +54,7 @@ public class MeepMeepTesting {
 
                                 .strafeLeft(4)
                                 .splineToConstantHeading(new Vector2d(-63, -12), Math.toRadians(180))
-                                .waitSeconds(.5) //hello! //goodbye!
+                                .waitSeconds(.5) 
 
                                 .build()
 
