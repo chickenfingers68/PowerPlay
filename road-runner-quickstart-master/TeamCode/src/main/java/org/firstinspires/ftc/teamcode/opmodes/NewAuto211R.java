1b5d0ad0f23b8db19cb4eeb0c70a02ca218fc1fd
chickenfingers68 <jackienguyen796@gmail.com>
2023-03-10 00:36:25 -0800

    i hate this

--- road-runner-quickstart-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/NewAuto211R.java
+++ road-runner-quickstart-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/NewAuto211R.java
@@ -41,7 +41,7 @@ import java.util.ArrayList;
  * to supercharge your code. This can be much cleaner by abstracting many of these things. This
  * opmode only serves as an initial starting point.
  */
-@Autonomous(name = "quitting robotics right", group = "adv")
+@Autonomous(name = "why am i the only programmer", group = "adv")
 public class NewAuto211R extends LinearOpMode {
 
     private Servo claw;
